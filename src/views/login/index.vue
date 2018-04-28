e20e1fa2b542915ab18bb81878e4c48ca8052bb0
lll <1076408188@qq.com>
2018-04-28 17:40:35 +0800

    提交test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,22 +2,32 @@
   <div class="login-container">
     <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
       class="card-box login-form">
+      <div class="logo">
+        <img  class="logo-img" src="http://192.168.1.25:8080/waimaibao/upload/1449647194.jpg" alt="">
+      </div>
       <h3 class="title">欢迎使用安发物流</h3>
        <el-form-item prop="username">
-        <span class="svg-container svg-container_login">
-          <icon-svg icon-class="yonghuming" />
-        </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="邮箱" />
+        <!--<span class="svg-container svg-container_login">-->
+          <!--<icon-svg icon-class="yonghuming" />-->
+        <!--</span>-->
+        <el-input class="username-input" name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="公司ID" />
       </el-form-item>
       <el-form-item prop="password">
-        <span class="svg-container">
-          <icon-svg icon-class="mima" ></icon-svg>
-        </span>
-        <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-          placeholder="密码"></el-input>
+        <!--<span class="svg-container">-->
+          <!--<icon-svg icon-class="mima" ></icon-svg>-->
+        <!--</span>-->
+        <el-input  class="password-input" name="" type="number" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
+          placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <!--<span class="svg-container">-->
+        <!--<icon-svg icon-class="mima" ></icon-svg>-->
+        <!--</span>-->
+        <el-input  class="password-input" name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
+                   placeholder="密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+        <el-button class="el-but" type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
           登录
         </el-button>
       </el-form-item>
@@ -89,7 +99,8 @@ export default {
   .login-container {
     @include relative;
     height: 100vh;
-    background-color: $bg;
+    background-color: rgb(24,54,80);
+
     input:-webkit-autofill {
       -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
       -webkit-text-fill-color: #fff !important;
@@ -100,7 +111,7 @@ export default {
       -webkit-appearance: none;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
-      color: $light_gray;
+      color: rgba(153, 153, 153, 0.5);
       height: 47px;
     }
     .el-input {
@@ -123,28 +134,43 @@ export default {
         font-size: 20px;
       }
     }
-    .title {
-      font-size: 26px;
-      font-weight: 400;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
+
     .login-form {
       position: absolute;
       left: 0;
       right: 0;
-      width: 400px;
-      padding: 35px 35px 15px 35px;
+      width: 468px;
+      padding: 25px 25px 32px 25px;
       margin: 120px auto;
+      background: #fff;
+
     }
     .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.1);
+      border: 1px solid #666;
+      background: #fff;
       border-radius: 5px;
       color: #454545;
+      margin-bottom: 16px !important;
     }
+    .logo{
+      display: inline-block;
+      text-align: center;
+      margin-left: 128px;
+    }
+    .logo-img{
+      display: inline-block;
+      width: 140px;
+      height: 50px;
+      vertical-align: middle;
+    }
+    .title {
+      font-size: 26px;
+      color: rgb(166,166,166);
+      margin: 40px auto 26px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+
     .show-pwd {
       position: absolute;
       right: 10px;
@@ -157,6 +183,9 @@ export default {
       position: absolute;
       right: 35px;
       bottom: 28px;
+    }
+    .el-but{
+      line-height: 66px !important;
     }
   }
 </style>
