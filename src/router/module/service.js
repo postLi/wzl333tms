748344646b270ca379d2ec23247141ab2bc60fee
何永青 <864887652@qq.com>
2018-05-11 15:27:22 +0800

    去除bootstrap； 修改边栏为自定义样式；

--- src/router/module/service.js
+++ src/router/module/service.js
@@ -12,7 +12,16 @@ export default {
     { path: '/service/index', name: '客服反馈', icon: 'zonghe', component: _import('service/index'), meta: { title: '客服反馈', noCache: true },
       children: [{
         path: '/service/index/longtime', name: '延时处理', component: _import('table/index'), meta: { role: ['admin'], title: '延时处理', noCache: true }
+      },
+      {
+        path: '/service/index/longtime2', name: '延时处理2', component: _import('table/index'), meta: { role: ['admin'], title: '延时处理2', noCache: true }
       }]
+    },
+    {
+      path: '/service/report', icon: 'baobiao', name: '报表管理', component: _import('report/index'), meta: { role: ['admin'], title: '报表管理', noCache: true }
+    },
+    {
+      path: '/service/report2', icon: 'baobiao', name: '报表管理2', component: _import('report/index'), meta: { role: ['admin'], title: '报表管理2', noCache: true }
     }
   ]
 }
--- src/views/layout/sidebar/SidebarItem.vue
+++ src/views/layout/sidebar/SidebarItem.vue
@@ -1,57 +1,27 @@
 <template>
-    <ul class="sidebar-menu" :default-active="$route.path">
+    <ul class="sidebar-menu" :default-active="$route.path"  ref="sidebarMenu">
       <SidebarItemNode
         class="menu-item"
         v-for="(route, index) in routes"
         :key="index"
+        v-if="!route.hidden"
+        @removeOtherOpenFolder="removeOtherOpenFolder"
+        ref="sidebarMenuItem"
         :routes="route">
       </SidebarItemNode>
-    <!-- <el-menu 
-      class="sidebar-menu" 
-      mode="vertical" 
-      theme="dark" 
-      :default-active="$route.path" 
-      :collapse="!sidebar.opened">
-
-    <template v-for="item in routes">
-      <router-link v-if="!item.hidden&&item.noDropdown&&item.children.length>0" :to="item.path" :key="item.name">
-        <el-menu-item :index="item.path">
-          <icon-svg v-if='item.icon' :icon-class="item.icon" /> <span class="sidebar-nav-title">{{item.children[0].meta.title}}</span>
-          <span class="sidebar-nav-stitle">{{item.children[0].meta.stitle || item.children[0].meta.title}}</span>
-        </el-menu-item>
-      </router-link>
-      
-        <el-menu-item v-if="!item.hidden&&(!item.children || item.children.length===0)" :index="item.path" :key="item.name">
-          <router-link :to="item.path" >
-          <icon-svg v-if='item.icon' :icon-class="item.icon" /> <span class="sidebar-nav-title">{{item.meta.title}}</span>
-          <span class="sidebar-nav-stitle">{{item.meta.stitle || item.meta.title}}</span>
-          </router-link>
-        </el-menu-item>
-      
-      <el-submenu :index="item.name" v-if="!item.noDropdown&&!item.hidden&&item.children&&item.children.length" class="sidebar-sub-menu" :key="item.name">
-        <template slot="title">
-          <router-link :to="item.path" :key="item.name">
-          <icon-svg v-if='item.icon' :icon-class="item.icon" /> <span class="sidebar-nav-title">{{item.meta.title}}</span>
-          <span class="sidebar-nav-stitle">{{item.meta.stitle || item.meta.title}}</span>
-          </router-link>
-        </template>
-        <template v-for="child in item.children" v-if='!child.hidden'>
-          <sidebar-item class='menu-indent' v-if='child.children&&child.children.length>0' :routes='[child]' :key="child.name"> </sidebar-item>
-          <router-link v-else class="menu-indent" :to="child.path" :key="child.name">
-            <el-menu-item :index="child.path">
-              {{child.meta.title}}
-            </el-menu-item>
-          </router-link>
-        </template>
-      </el-submenu>
-    </template>
-    </el-menu> -->
     </ul>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import SidebarItemNode from './SidebarItemNode'
+
+function hasParent(el,clsName) {
+  if(el.parentNode.classList.contain(clsName)){
+    el.parentNode.classList.add('clsName')
+  }
+}
+
 export default {
   name: 'SidebarItem',
   components: {
@@ -68,6 +38,16 @@ export default {
       'sidebarRouters',
       'permission_routers'
     ])
+  },
+  methods: {
+    removeOtherOpenFolder () {
+      let childRef = Array.isArray(this.$refs.sidebarMenuItem) ? this.$refs.sidebarMenuItem : [this.$refs.sidebarMenuItem]
+      childRef.map( el =>{
+        if(el.isFolder && el.open){
+          el.open = false
+        }
+      })
+    }
   }
 }
 </script>
@@ -76,52 +56,114 @@ export default {
 @import "src/styles/variate.scss";
 .sidebar-menu{
   color: rgba(255, 255, 255, 0.8);
-  &>.menu-item,&>.submenu{
+  &>.menu-item,&>.sidebar-submenu{
     border-left: 4px solid #002039;
+    padding-left: 20px;
+  }
+  .sidebar-submenu{
+    display: none;
   }
 
-  .el-menu-item, .el-submenu__title{
-    height: 44px;
-    line-height: 44px;
+  a{
+    display: block;
+  }
+
+  .isOpen>.sidebar-submenu{
+    display: block;
+  }
+
+  .menu-item{
+    height: 40px;
+    line-height: 40px;
     font-size: $sidebarFontSize;
     color: rgba(255, 255, 255, 0.8);
+    position: relative;
+    transition: height .4s ease;
+  }
+  .isOpen{
+    height: auto;
   }
 
   .sidebar-nav-title{
     display: inline-block;
   }
 
-  .sidebar-nav-stitle{
-    display: none;
-  }
-
-  .el-submenu .el-menu-item{
+  .sidebar-submenu .submenu-item{
     min-width: 100%;
-    padding-left: 60px !important;
+    padding-left: $sidebarFontSize * 1.2 + 10px;
     font-size: $sidebarFontSize - 2px;
   }
 
-  .is-active{
+  &>.is-active, &>.isOpen{
     border-left: 4px solid #104193;
     background: #031523;
-    color: #3e9ff1;
 
     .is-active{
       border: none;
     }
+  }
 
-    .el-submenu__title {
+  .is-active{
+    &>a {
       color: #3e9ff1;
     }
   }
-  .el-menu-item:focus, .el-menu-item:hover, .el-submenu__title:hover{
+
+  
+
+
+  .menu-item:focus, .menu-item:hover, .sidebar-nav-title:hover{
     background: #031523;
+    border-left-color: #104193;
+  }
+  .svg-icon {
+    margin-right: 6px;
+    font-size: 1.2em;
+    color: #fff;
+  }
+  .dropdownIcon{
+    position: absolute;
+    top: 12px;
+    right: 5px;
+    cursor: pointer;
   }
 }
-#app .el-menu--collapse.sidebar-menu{
-  .el-menu-item,.el-submenu{
-    position: relative;
-    padding-top: 10px;
+.hideSidebar .sidebar-menu{
+  position: relative;
+  z-index: 99;
+
+  .menu-item{
+    padding-top: 5px;
+    padding-bottom: 25px;
+    height: auto;
+    font-size: 12px;
+    min-height: 40px;
+    margin-bottom: 10px;
+
+    &>a>.sidebar-nav-title{
+      line-height: 20px;
+      min-height: auto;
+    }
+  }
+  .isOpen{
+    .sidebar-submenu{
+      display: none;
+    }
+  }
+  .menu-item:hover{
+    .sidebar-submenu{
+      background: #031523;
+      display: block;
+    }
+  }
+  .svg-icon{
+    margin-left: -4px;
+  }
+  .sidebar-submenu{
+    position: absolute;
+    left: $hideSidebarWidth - 4px;
+    top: 0;
+    width: 150px;
   }
   .el-menu--popup{
     background: #031523;
@@ -129,22 +171,27 @@ export default {
   .el-submenu__icon-arrow{
     display: none;
   }
-  .sidebar-nav-title{
+
+  a:hover{
+    color: #3e9ff1;
+  }
+
+  .dropdownIcon{
     display: none;
   }
-  .sidebar-nav-stitle{
-    display: block;
+
+  .menu-item>a>.sidebar-nav-title{
     position: absolute;
     left: 0;
     width: 100%;
-    top: 14px;
+    top: 20px;
     text-align: center;
     line-height: 22px;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
   }
-  .el-menu-item, .el-submenu__title{
+  .el-menu-item, .sidebar-nav-title{
     min-height: 44px;
     height: auto;
   }
@@ -154,13 +201,6 @@ export default {
   .el-submenu .el-menu-item{
     padding-left: 20px !important;
   }
-}
-.svg-icon {
-  margin-right: 10px;
-}
-.hideSidebar .menu-indent{
-  display: block;
-  text-indent: 10px;
 }
 
 </style>
