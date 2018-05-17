5c8d92593b7331ee6d36660671fe7d6b19e3e364
lll <1076408188@qq.com>
2018-05-17 09:10:06 +0800

    更新

---
+++ src/api/company/permissionManage.js
@@ -0,0 +1,32 @@
+import fetch from '../../utils/fetch'
+
+/**
+ * 根据网点id查询该公司的所有角色信息
+ * @param orgid
+ * @param roleName
+ * @param pageSize
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export function getroleInfo(orgid, pageSize, roleName) {
+  return fetch.get('/api-system/system/role/v1/',{
+    params:{orgid,roleName,pageSize}
+  }).then(res=>{
+    return res.data?res.data.list:[]
+  })
+}
+/**
+ * 获取指定网点的权限信息
+ * @param {*} orgid 网点id
+ * @param {*} pagesize 获取权限列表的长度
+ */
+export function getAuthInfo(orgid, pagesize) {
+  return fetch.get('/api-system/system/role/v1/', {
+    params: {
+      orgid,
+      roleName: '',
+      pageSize: pagesize || 50
+    }
+  }).then(res => {
+    return res.data ? res.data.list : []
+  })
+}
