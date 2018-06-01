faf3505b6e1df42d37867098a297981b62e5958d
postLi <1285161341@qq.com>
2018-06-01 09:48:24 +0800

    修复路由错误

--- src/router/module/finance.js
+++ src/router/module/finance.js
@@ -8,8 +8,6 @@ export default {
   name: '财务管理',
   icon: 'caiwu',
   children: [
-        { path: '/finance/index', icon: 'information', hidden: true, name: '财务中心', component: _import('finance/index'), meta: { title: '财务中心', noCache: true }},
-        { path: '/finance/FinancialProcesses', icon: 'information', hidden: false, name: '财务流程', component: _import('finance/FinancialProcesses/index'), meta: { title: '财务流程', noCache: true }},
-        { path: '/finance/WriteOff', icon: 'information', hidden: false, name: '核销管理', component: _import('finance/WriteOff/index'), meta: { title: '核销管理', noCache: true }}
+        { path: '/finance/index', icon: 'information', hidden: true, name: '财务中心', component: _import('finance/index'), meta: { title: '财务中心', noCache: true }}
   ]
 }
