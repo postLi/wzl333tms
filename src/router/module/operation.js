623a0c77c47539f29f942afa1f49c479736ddb9c
何永青 <864887652@qq.com>
2018-06-04 13:51:23 +0800

    调整路由数据

--- src/router/module/operation.js
+++ src/router/module/operation.js
@@ -18,37 +18,40 @@ export default {
     path: '/operation/order/manage', icon: 'QQ', name: '订单管理', component: _import('operation/order/manage/index'), meta: { role: ['admin'], title: '订单管理', noCache: true }
   },
   {
-    path: '/operation/receipt', icon: 'QQ', name: '回单管理', component: _import('operation/order/receipt/index'), meta: { role: ['admin'], title: '回单管理', noCache: true }
+    path: '/operation/order/receipt', icon: 'QQ', name: '回单管理', component: _import('operation/order/receipt/index'), meta: { role: ['admin'], title: '回单管理', noCache: true }
   },
   {
-    path: '/operation/pickUp', icon: 'QQ', name: '提货管理', component: _import('operation/order/pickUp/index'), meta: { role: ['admin'], title: '提货管理', noCache: true }
+    path: '/operation/order/pickUp', icon: 'QQ', name: '提货管理', component: _import('operation/order/pickUp/index'), meta: { role: ['admin'], title: '提货管理', noCache: true }
   },
   {
-    path: '/operation/repertory', icon: 'QQ', name: '库存管理', component: _import('operation/order/repertory/index'), meta: { role: ['admin'], title: '库存管理', noCache: true }
+    path: '/operation/order/repertory', icon: 'QQ', name: '库存管理', component: _import('operation/order/repertory/index'), meta: { role: ['admin'], title: '库存管理', noCache: true }
   },
   {
-    path: '/operation/shortDepart', icon: 'QQ', name: '短驳管理', component: _import('operation/order/shortDepart/index'), meta: { role: ['admin'], title: '短驳管理', noCache: true }
+    path: '/operation/order/shortDepart', icon: 'QQ', name: '短驳管理', component: _import('operation/order/shortDepart/index'), meta: { role: ['admin'], title: '短驳管理', noCache: true }
   },
   {
-    path: '/operation/arteryDepart', icon: 'QQ', name: '干线发车', component: _import('operation/order/arteryDepart/index'), meta: { role: ['admin'], title: '干线发车', noCache: true }
+    path: '/operation/order/arteryDepart', icon: 'QQ', name: '干线发车', component: _import('operation/order/arteryDepart/index'), meta: { role: ['admin'], title: '干线发车', noCache: true }
   },
   {
-    path: '/operation/arteryDelivery', icon: 'QQ', name: '干线到车', component: _import('operation/order/arteryDelivery/index'), meta: { role: ['admin'], title: '干线到车', noCache: true }
+    path: '/operation/order/arteryDelivery', icon: 'QQ', name: '干线到车', component: _import('operation/order/arteryDelivery/index'), meta: { role: ['admin'], title: '干线到车', noCache: true }
   },
   {
-    path: '/operation/deliverManage', icon: 'QQ', name: '送货管理', component: _import('operation/order/deliverManage/index'), meta: { role: ['admin'], title: '送货管理', noCache: true }
+    path: '/operation/order/deliverManage', icon: 'QQ', name: '送货管理', component: _import('operation/order/deliverManage/index'), meta: { role: ['admin'], title: '送货管理', noCache: true }
   },
   {
-    path: '/operation/track', icon: 'QQ', name: '在途跟踪', component: _import('operation/order/track/index'), meta: { role: ['admin'], title: '在途跟踪', noCache: true }
+    path: '/operation/order/track', icon: 'QQ', name: '在途跟踪', component: _import('operation/order/track/index'), meta: { role: ['admin'], title: '在途跟踪', noCache: true }
   },
   {
-    path: '/operation/sign', icon: 'QQ', name: '签收管理', component: _import('operation/order/sign/index'), meta: { role: ['admin'], title: '签收管理', noCache: true }
+    path: '/operation/order/sign', icon: 'QQ', name: '签收管理', component: _import('operation/order/sign/index'), meta: { role: ['admin'], title: '签收管理', noCache: true }
   },
   {
-    path: '/operation/transfer', icon: 'QQ', name: '中转管理', component: _import('operation/order/transfer/index'), meta: { role: ['admin'], title: '中转管理', noCache: true }
+    path: '/operation/order/transfer', icon: 'QQ', name: '中转管理', component: _import('operation/order/transfer/index'), meta: { role: ['admin'], title: '中转管理', noCache: true }
   },
   {
     path: '/operation/order/pickClip', hidden: true, icon: 'QQ', name: '挑单夹', component: _import('operation/order/pickClip/index'), meta: { role: ['admin'], title: '挑单夹', noCache: true }
+  },
+  {
+    path: '/operation/order/orderDetail', hidden: true, icon: 'QQ', name: '订单详情', component: _import('operation/order/orderDetail/index'), meta: { role: ['admin'], title: '订单详情', noCache: true }
   },
   {
     path: '/operation/service',
