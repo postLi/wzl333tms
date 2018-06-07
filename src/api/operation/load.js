a8ac75e5a727ddfa1558da2b52cc1be92f645344
lyy <lyy6217@163.com>
2018-06-07 18:40:24 +0800

    提交load.js

--- src/api/operation/load.js
+++ src/api/operation/load.js
@@ -2,4 +2,29 @@ import fetch from '../../utils/fetch'
 /**
  * 新增配载
  */
-
+/**
+ * 获得配载批次号
+ */
+export function getBatchNo(id) {
+  return fetch.get('/api-order/order/load/v1/getBatchNo', {
+    params: {
+      orgId: id
+    }
+  })
+}
+/**
+ * 新增时配载左边的库存列表
+ */
+export function getSelectAddLoadRepertoryList(id) {
+  return fetch.get('/api-order/order/load/v1/selectAddLoadRepertoryList/', {
+    params: {
+      orgId: id
+    }
+  })
+}
+/**
+ * 插入配载信息
+ */
+export function postLoadInfo(data) {
+  return fetch.post('/api-order/order/load/v1/', data)
+}
