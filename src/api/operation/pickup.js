df7ea600dd7b995d95363fa3072a51c577c7df92
lll <1076408188@qq.com>
2018-06-08 18:39:49 +0800

    提交

--- src/api/operation/pickup.js
+++ src/api/operation/pickup.js
@@ -10,3 +10,172 @@ export function fetchPostlist(params) {
     return res.data || {}
   })
 }
+
+/**
+ * 自动获取提货批次
+ * @returns {AxiosPromise<any>}
+ */
+
+export function fetchGetPickUp() {
+  return fetch.get('/api-order/order/pickup/v1/getPickupBatch')
+}
+// /**
+//  *
+//  */
+// private Integer id;
+// /**
+//  * 提货批次
+//  */
+// private String pickupBatchNumber;
+// /**
+//  * 提货状态
+//  */
+// private Integer pickupStatus;
+// /**
+//  * 运费核销状态
+//  */
+// private Integer cancelStatus;
+// /**
+//  * 发货人id
+//  */
+// private Integer senderId;
+// /**
+//  * 货品名
+//  */
+// private String pickupName;
+// /**
+//  * 件数
+//  */
+// private Integer pickupAmount;
+// /**
+//  * 重量
+//  */
+// private Double pickupWeight;
+// /**
+//  * 体积
+//  */
+// private Double pickupVolume;
+// /**
+//  * 实际载重量
+//  */
+// private Double realWeight;
+// /**
+//  * 差额重量
+//  */
+// private Double differWeight;
+// /**
+//  * 实际载体积
+//  */
+// private Double realVolume;
+// /**
+//  * 差额体积
+//  */
+// private Double differVolume;
+// /**
+//  * 运单id
+//  */
+// private String orderId;
+// /**
+//  * 关联提货费
+//  */
+// private Double pickupFee;
+// /**
+//  * 到达城市
+//  */
+// private String toCityCode;
+// /**
+//  * 付款方式
+//  */
+// private Integer payMethod;
+// /**
+//  * 备注
+//  */
+// private String remark;
+// /**
+//  * 运费
+//  */
+// private BigDecimal carriage;
+// /**
+//  * 代收费用
+//  */
+// private BigDecimal collectionFee;
+// /**
+//  * 车辆id
+//  */
+// private Integer truckId;
+// /**
+//  * 车费
+//  */
+// private BigDecimal truckFee;
+// /**
+//  * 出车时间
+//  */
+// @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+// private Date outTime;
+// /**
+//  * 要求到车时间
+//  */
+// @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+// private Date arriveTime;
+//
+// /**
+//  * 开单网点
+//  */
+// private Integer pickupFromOrgid;
+//
+// /*发货人*/
+// /**
+//  * 发货人
+//  */
+// private String senderName;
+// /**
+//  * 发货人电话
+//  */
+// private String senderMobile;
+//
+// /**
+//  * 发货人地址
+//  */
+// private String detailedAddress;
+// /*车辆信息*/
+// /**
+//  * 车牌号码
+//  */
+// private String truckIdNumber;
+// /**
+//  * 车辆单位
+//  */
+// private String truckUnit;
+// /**
+//  * 车型
+//  */
+// private Integer truckType;
+// /*司机信息*/
+// /**
+//  * 司机姓名
+//  */
+// private String driverName;
+// /**
+//  * 司机电话
+//  */
+// private String driverMobile;
+// /**
+//  * 本网点及子网点id
+//  */
+// private  String orgAllId;
+//
+//
+// /**
+//  * 运单单号s
+//  */
+// private String shipSns;
+//
+// /**
+//  * 运单号
+//  */
+// private String shipSn;
+//
+// /**
+//  * 货号
+//  */
+// private String shipGoodsSn;
