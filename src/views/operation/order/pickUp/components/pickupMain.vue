e51ffbb62a697928e9a6ef51efca16f8bc54df54
lll <1076408188@qq.com>
2018-06-12 19:57:34 +0800

    删除-提货管理无关文件

---
+++ src/views/operation/order/pickUp/components/pickupMain.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="dep-maintain">
+    <PopFrame :title="popTitle" :isShow="popVisible" @close="closeMe" class='pickpopDepMain' v-loading="loading">
+      <template class='pickRelationPop-content' slot="content">
+        <!--isDepMain-->
+        <div ref="ruleForm" class="depmain-div">
+
+            <div class="pick_center">是否生成托运单？</div>
+            <div class="pick_input">
+              生成票数
+              <el-input v-model="input" placeholder="请输入内容"></el-input>
+            </div>
+        </div>
+      </template>
+      <div slot="footer" class="dialog-footer-frame" >
+        <el-button type="primary" @click="submitForm('ruleForm')">生成</el-button>
+        <el-button @click="closeMe">不生成</el-button>
+      </div>
+
+    </PopFrame>
+  </div>
+
+</template>
+
+<script>
+    import PopFrame from '@/components/PopFrame/index'
+    import { getSelectDictInfo,postDict,deletePerManage,putDict } from '@/api/company/groupManage'
+    export default {
+      components: {
+        PopFrame
+      },
+      props: {
+        popVisible: {
+          type: Boolean,
+          default: false
+        },
+        dotInfo: [Object,Array],
+        isDepMain:{
+          type:Boolean,
+          default:false
+        },
+        createrId: [Number,String]
+      },
+      data() {
+        return {
+          checked1: true,
+          popTitle: '提示',
+          loading:false,
+          getMentInfo:[
+            {dictName:'',id:''}
+          ],
+          //首行
+          input: '',
+          orderId: '',
+          resInfo:[
+            {
+              dictName:''
+            }
+          ],
+          restaurants: [],
+        }
+      },
+      computed: {
+        isShow: {
+          get(){
+            return this.popVisible
+          },
+          set(){
+
+          }
+        }
+      },
+      watch: {
+        isDepMain(){
+          if(this.isDepMain){
+
+          }else{
+          }
+        },
+        dotInfo (newVal) {
+          // this.getMentInfo = this.dotInfo
+        },
+        popVisible (newVal) {
+          console.log('popVisible:', newVal)
+        },
+        createrId(newVal){
+
+        }
+      },
+      mounted() {
+        this.getSelectDict(this.createrId)
+      },
+      methods: {
+        getSelectDict(orgId) {
+          //
+          // this.loading = true
+          // getSelectDictInfo(this.createrId).then(res => {
+          //   this.loading = false
+          //   this.getMentInfo = res
+          // })
+
+        },
+        getAddDate() {
+          // this.loading = true
+          // return postDict(this.createrId , this.dictName).then(res => {
+          //   this.loading = false
+          //   this.dictName = ''
+          // })
+
+        },
+        closeMe (done) {
+          this.$emit('update:popVisible', false);
+          if(typeof done === 'function'){
+            done()
+          }
+        },
+        submitForm(formName) {
+          this.$message({
+            message: '开单页面~',
+            type: 'warning'
+          })
+          // this.$refs[formName].validate((valid) => {
+          //   if (valid) {
+          //     this.loading = true
+          //     this.form.tmsCustomer = this.customSend
+          //     this.form.tmsOrderPickup.outTime = this.newDate
+          //     this.form.tmsOrderPickup.arriveTime = this.endDate
+          //     // console.log(this.form)
+          //     let data = this.form
+          //     // let data = Object.assign({},this.form)
+          //     // data.fixPhone = this.fixPhone
+          //     let promiseObj
+          //     // 判断操作，调用对应的函数
+          //     if(this.isModify){
+          //       // promiseObj = putUpdatePickup(data)
+          //     } else {
+          //       // promiseObj = postAddPickup(data)
+          //     }
+          //
+          //     promiseObj.then(res => {
+          //       this.loading = false
+          //       this.$alert('操作成功', '提示', {
+          //         confirmButtonText: '确定',
+          //         callback: action => {
+          //           this.closeMe()
+          //           this.$emit('success')
+          //         }
+          //       });
+          //     }).catch(err => {
+          //       this.loading = false
+          //     })
+          //   } else {
+          //     return false;
+          //   }
+          // });
+        },
+      }
+    }
+</script>
+
+<style lang="scss">
+  .dep-maintain .pickpopDepMain{
+    top: 29%;
+    bottom: auto;
+    min-width: 390px;
+    max-width:  390px;
+
+  }
+  .dep-maintain .pickRelationPop-content{
+    padding: 20px 20px 0;
+    box-sizing: border-box;
+
+  }
+.pick_center{
+  text-align: center;
+  padding-top: 80px;
+  font-size: 16px;
+}
+  .pick_input{
+    padding-top: 60px;
+    text-align: center;
+    font-size: 14px;
+    .el-input{
+      width: 40%;
+
+    }
+  }
+</style>
---
+++ src/views/operation/order/pickUp/components/pickupRelevance.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="pick-maintain">
+    <PopFrame :title="popTitle" :isShow="popVisible" @close="closeMe" class='pickpopDepMain' v-loading="loading">
+      <template class='pickRelationPop-content' slot="content">
+        <!--isDepMain-->
+        <div ref="ruleForm" class="depmain-div">
+          <el-form :inline="true" :model="formInline" class="demo-form-inline" :label-width="formLabelWidth">
+            <el-form-item label="提货批次">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="派车费用">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="车牌号码">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="司机姓名">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+          </el-form>
+          <el-table
+            ref="multipleTable"
+            :data="usersArr"
+            stripe
+            border
+            @row-click="clickDetails"
+            @selection-change="getSelection"
+            height="100%"
+            tooltip-effect="dark"
+            :default-sort = "{prop: 'id', order: 'ascending'}"
+            style="width: 100%">
+            <el-table-column
+              fixed
+              sortable
+              type="selection"
+              width="78">
+            </el-table-column>
+            <el-table-column
+              fixed
+              sortable
+              prop="customerId"
+              label="运单号"
+              width="160">
+            </el-table-column>
+            <el-table-column
+              fixed
+              sortable
+              prop="pickupBatchNumber"
+              width="150"
+              label="货号">
+            </el-table-column>
+            <el-table-column
+              fixed
+              sortable
+              prop="pickupBatchNumber"
+              width="150"
+              label="实际提货费">
+            </el-table-column>
+          </el-table>
+
+          <el-form :inline="true" :model="formInline" class="order_bottom" :label-width="formLabelWidth">
+            <el-form-item label="运单号">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="货号">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="本单提货费">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+          </el-form>
+
+        </div>
+      </template>
+      <div slot="footer" class="dialog-footer-frame" >
+        <el-button type="primary" @click="submitForm('ruleForm')">生成</el-button>
+        <el-button @click="closeMe">不生成</el-button>
+      </div>
+
+    </PopFrame>
+  </div>
+
+</template>
+
+<script>
+  import PopFrame from '@/components/PopFrame/index'
+  import { getSelectDictInfo,postDict,deletePerManage,putDict } from '@/api/company/groupManage'
+  export default {
+    components: {
+      PopFrame
+    },
+    props: {
+      popVisible: {
+        type: Boolean,
+        default: false
+      },
+      dotInfo: [Object,Array],
+      isDepMain:{
+        type:Boolean,
+        default:false
+      },
+      createrId: [Number,String]
+    },
+    data() {
+      return {
+        formLabelWidth:'90',
+        formInline: {
+          user: '',
+          region: ''
+        },
+        checked1: true,
+        popTitle: '关联运单',
+        loading:false,
+        getMentInfo:[
+          {dictName:'',id:''}
+        ],
+        //首行
+        input: '',
+        orderId: '',
+        resInfo:[
+          {
+            dictName:''
+          }
+        ],
+        restaurants: [],
+      }
+    },
+    computed: {
+      isShow: {
+        get(){
+          return this.popVisible
+        },
+        set(){
+
+        }
+      }
+    },
+    watch: {
+      isDepMain(){
+        if(this.isDepMain){
+
+        }else{
+        }
+      },
+      dotInfo (newVal) {
+        // this.getMentInfo = this.dotInfo
+      },
+      popVisible (newVal) {
+        console.log('popVisible:', newVal)
+      },
+      createrId(newVal){
+
+      }
+    },
+    mounted() {
+      this.getSelectDict(this.createrId)
+    },
+    methods: {
+      getSelectDict(orgId) {
+        //
+        // this.loading = true
+        // getSelectDictInfo(this.createrId).then(res => {
+        //   this.loading = false
+        //   this.getMentInfo = res
+        // })
+
+      },
+      getAddDate() {
+        // this.loading = true
+        // return postDict(this.createrId , this.dictName).then(res => {
+        //   this.loading = false
+        //   this.dictName = ''
+        // })
+
+      },
+      closeMe (done) {
+        this.$emit('update:popVisible', false);
+        if(typeof done === 'function'){
+          done()
+        }
+      },
+      submitForm(formName) {
+        this.$message({
+          message: '开单页面~',
+          type: 'warning'
+        })
+        // this.$refs[formName].validate((valid) => {
+        //   if (valid) {
+        //     this.loading = true
+        //     this.form.tmsCustomer = this.customSend
+        //     this.form.tmsOrderPickup.outTime = this.newDate
+        //     this.form.tmsOrderPickup.arriveTime = this.endDate
+        //     // console.log(this.form)
+        //     let data = this.form
+        //     // let data = Object.assign({},this.form)
+        //     // data.fixPhone = this.fixPhone
+        //     let promiseObj
+        //     // 判断操作，调用对应的函数
+        //     if(this.isModify){
+        //       // promiseObj = putUpdatePickup(data)
+        //     } else {
+        //       // promiseObj = postAddPickup(data)
+        //     }
+        //
+        //     promiseObj.then(res => {
+        //       this.loading = false
+        //       this.$alert('操作成功', '提示', {
+        //         confirmButtonText: '确定',
+        //         callback: action => {
+        //           this.closeMe()
+        //           this.$emit('success')
+        //         }
+        //       });
+        //     }).catch(err => {
+        //       this.loading = false
+        //     })
+        //   } else {
+        //     return false;
+        //   }
+        // });
+      },
+      clickDetails(row, event, column){
+        this.$refs.multipleTable.toggleRowSelection(row)
+      },
+      getSelection (selection) {
+        this.selected = selection
+      },
+    }
+  }
+</script>
+
+<style lang="scss">
+  .pick-maintain .pickpopDepMain{
+    top: 29%;
+    bottom: auto;
+    min-width: 550px;
+    max-width:  550px;
+    min-height: 520px;
+    max-height:  520px;
+
+  }
+  .pick-maintain .popRight-content{
+    padding: 20px 0 20px 10px;
+    box-sizing: border-box;
+    .el-form--inline .el-form-item{
+      margin-right: -8px;
+    }
+    .el-input{
+      width: 90%;
+    }
+    .order_bottom{
+      .el-form-item{
+
+      }
+      .el-input{
+        width: 50%;
+
+      }
+    }
+  }
+  .pick_center{
+    text-align: center;
+    padding-top: 80px;
+    font-size: 16px;
+  }
+  .pick_input{
+    padding-top: 60px;
+    text-align: center;
+    font-size: 14px;
+    .el-input{
+      width: 40%;
+
+    }
+  }
+</style>
