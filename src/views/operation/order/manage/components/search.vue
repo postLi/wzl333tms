deb0fa4afc9d61d45387238c0ae38d694525931c
lll <1076408188@qq.com>
2018-07-06 14:02:51 +0800

    提交

--- src/views/operation/order/manage/components/search.vue
+++ src/views/operation/order/manage/components/search.vue
@@ -80,13 +80,15 @@ export default {
       this.searchForm.orgid = id
     },
     onSubmit () {
-      this.searchForm.createTime = this.searchCreatTime[0]
-      this.searchForm.endTime = this.searchCreatTime[1]
+      this.searchForm.createTime = this.searchCreatTime[0] ? +this.searchCreatTime[0] : ''
+      this.searchForm.endTime = this.searchCreatTime[1] ? +this.searchCreatTime[1] : ''
       this.$emit('change', this.searchForm)
+      console.log(this.searchForm);
     },
     clearForm () {
       this.searchForm.createTime = ''
       this.searchForm.endTime = ''
+      this.searchCreatTime = []
       this.searchForm.orderStatus = ''
       this.searchForm.orderSn = ''
     }
