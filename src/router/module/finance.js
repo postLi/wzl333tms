3e329cd84d6b3ccf9192a377fa0eddd37bfadb25
何永青 <864887652@qq.com>
2018-07-13 17:48:19 +0800

    更新路由图标

--- src/router/module/finance.js
+++ src/router/module/finance.js
@@ -12,16 +12,16 @@ export default {
       path: '/finance/index', icon: 'QQ', hidden: true, name: '财务管理', component: _import('finance/index'), meta: { role: ['admin'], title: '财务管理', noCache: false }
     },
     {
-      path: '/finance/handAccount', icon: 'QQ', name: '员工交账', component: _import('finance/handAccount/index'), meta: { role: ['admin'], title: '员工交账', noCache: false }
+      path: '/finance/handAccount', icon: 'caiwugl4_yuangongjz', name: '员工交账', component: _import('finance/handAccount/index'), meta: { role: ['admin'], title: '员工交账', noCache: false }
     },
     {
       path: '/finance/handAccount/detail', icon: 'QQ', name: '交账明细', component: _import('finance/handAccount/detail'), meta: { role: ['admin'], title: '交账明细', noCache: false }
     },
     {
-      path: '/finance/accountsReceivable', icon: 'QQ', name: '应收账款', component: _import('finance/accountsReceivable/index'), meta: { role: ['admin'], title: '应收账款', noCache: false }
+      path: '/finance/accountsReceivable', icon: 'caiwugl1_yingshou', name: '应收账款', component: _import('finance/accountsReceivable/index'), meta: { role: ['admin'], title: '应收账款', noCache: false }
     },
     {
-      path: '/finance/accountsPayable', icon: 'QQ', name: '应付账款', component: _import('finance/accountsPayable/index'), redirect: '/finance/handAccount/waybill', meta: { role: ['admin'], title: '应付账款', istab: true, noCache: false },
+      path: '/finance/accountsPayable', icon: 'caiwugl2_yingfu', name: '应付账款', component: _import('finance/accountsPayable/index'), redirect: '/finance/handAccount/waybill', meta: { role: ['admin'], title: '应付账款', istab: true, noCache: false },
       children: [
         {
           path: '/finance/accountsPayable/waybill', icon: 'QQ', name: '运单结算', component: _import('finance/accountsPayable/waybill/index'), meta: { role: ['admin'], title: '运单结算', noCache: false }
@@ -35,13 +35,13 @@ export default {
       ]
     },
     {
-      path: '/finance/settleLog', icon: 'QQ', name: '资金流水汇总', component: _import('finance/settleLog/index'), meta: { role: ['admin'], title: '资金流水汇总', noCache: false }
+      path: '/finance/settleLog', icon: 'caiwugl5_zijinls', name: '资金流水汇总', component: _import('finance/settleLog/index'), meta: { role: ['admin'], title: '资金流水汇总', noCache: false }
     },
     {
-      path: '/finance/IOManage', icon: 'QQ', name: '收支方式管理', component: _import('finance/IOManage/index'), meta: { role: ['admin'], title: '收支方式管理', noCache: false }
+      path: '/finance/IOManage', icon: 'caiwugl3_shouzhifs', name: '收支方式管理', component: _import('finance/IOManage/index'), meta: { role: ['admin'], title: '收支方式管理', noCache: false }
     },
     {
-      path: '/finance/reconciliation', icon: 'QQ', redirect: '/finance/reconciliation/carrier', name: '财务对账', component: _import('finance/reconciliation/index'), meta: { role: ['admin'], title: '财务对账', noCache: false, istab: true },
+      path: '/finance/reconciliation', icon: 'caiwugl6_caiwudz', redirect: '/finance/reconciliation/carrier', name: '财务对账', component: _import('finance/reconciliation/index'), meta: { role: ['admin'], title: '财务对账', noCache: false, istab: true },
       children: [
         {
           path: '/finance/reconciliation/carrier', icon: 'QQ', name: '承运商对账', component: _import('finance/reconciliation/carrier/index'), meta: { role: ['admin'], title: '承运商对账', noCache: false }
