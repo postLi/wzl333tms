2678a9d1b6d41b7f3988b0cc4e3aaaafac910c2f
何永青 <864887652@qq.com>
2018-07-16 08:49:55 +0800

    应收账款

---
+++ src/api/finance/accountsReceivable.js
@@ -0,0 +1,421 @@
+import fetch from '@/utils/fetch'
+
+/**
+ * ================================》应收《================
+ */
+
+/**
+ * 插入运单相关的应收应付表信息
+ {
+  "tmsOrderCargoList": [
+    {
+      "agencyFund": 0,
+      "amountFee": 0,
+      "brokerageFee": 0,
+      "cargoAmount": 0,
+      "cargoName": "string",
+      "cargoPack": "string",
+      "cargoRemark": "string",
+      "cargoVolume": 0,
+      "cargoWeight": 0,
+      "commissionFee": 0,
+      "createTime": "2018-07-15T12:36:02.059Z",
+      "customsFee": 0,
+      "deliveryFee": 0,
+      "description": "string",
+      "forkliftFee": 0,
+      "goupstairsFee": 0,
+      "handlingFee": 0,
+      "housingFee": 0,
+      "id": 0,
+      "insuranceFee": 0,
+      "orderId": 0,
+      "orgid": 0,
+      "otherfeeIn": 0,
+      "otherfeeOut": 0,
+      "packageFee": 0,
+      "pickupFee": 0,
+      "productPrice": 0,
+      "realityhandlingFee": 0,
+      "shipFee": 0,
+      "shipId": 0,
+      "stampTax": 0,
+      "taxRate": 0,
+      "taxes": 0,
+      "volumeFee": 0,
+      "weightFee": 0
+    }
+  ],
+  "tmsOrderShip": {
+    "createTime": "2018-07-15T12:36:02.059Z",
+    "id": 0,
+    "shipArrivepayFee": 0,
+    "shipBatchId": 0,
+    "shipBusinessType": 0,
+    "shipCustomerNumber": "string",
+    "shipDelete": 0,
+    "shipDeliveryMethod": 0,
+    "shipEffective": 0,
+    "shipFromCityCode": "string",
+    "shipFromCityName": "string",
+    "shipFromOrgid": 0,
+    "shipGoodsSn": "string",
+    "shipIsAbnormal": 0,
+    "shipIsControll": 0,
+    "shipIsSeparate": 0,
+    "shipIsTransfer": 0,
+    "shipIsUpdate": 0,
+    "shipMonthpayFee": 0,
+    "shipNowpayFee": 0,
+    "shipOther": "string",
+    "shipPayWay": 0,
+    "shipPrintLib": 0,
+    "shipReceiptNum": 0,
+    "shipReceiptRequire": 0,
+    "shipReceiptSn": "string",
+    "shipReceiptpayFee": 0,
+    "shipReceiverId": 0,
+    "shipRemarks": "string",
+    "shipSenderId": 0,
+    "shipShippingType": 0,
+    "shipSn": "string",
+    "shipStatus": 0,
+    "shipToCityCode": "string",
+    "shipToCityName": "string",
+    "shipToOrgid": 0,
+    "shipTotalFee": 0,
+    "shipTruckIdNumber": "string",
+    "shipUserid": 0
+  },
+  "tmsOrderTransfer": {
+    "arrivalMobile": "string",
+    "carrierId": 0,
+    "carrierMobile": "string",
+    "childShipId": 0,
+    "codService": 0,
+    "createTime": "2018-07-15T12:36:02.059Z",
+    "deliveryExpense": 0,
+    "id": 0,
+    "oddNumbers": "string",
+    "orgid": 0,
+    "paymentId": 0,
+    "remark": "string",
+    "repertoryId": 0,
+    "shipId": 0,
+    "totalCost": 0,
+    "transferAmount": 0,
+    "transferBatchNo": "string",
+    "transferCharge": 0,
+    "transferOtherFee": 0,
+    "transferStatusId": 0,
+    "transferTime": "2018-07-15T12:36:02.059Z",
+    "transferVolume": 0,
+    "transferWeight": 0,
+    "updateTime": "2018-07-15T12:36:02.059Z"
+  },
+  "type": 0
+}
+ */
+export function postNewFeeReceivable(data) {
+  return fetch.post('/api-finance/finance/feeReceivable/v1/', data)
+}
+
+/**
+ * 根据ID查询tms应收应付表信息
+ * @param {*} id 应付id
+ */
+export function getFeeReceivable(id) {
+  return fetch.get('/api-finance/finance/feeReceivable/v1/' + id)
+}
+
+/**
+ * 根据ID修改tms应收应付表信息
+ * @param {*} data 修改信息
+{
+  "endTime": "2018-07-15T12:38:18.232Z",
+  "feeType": 0,
+  "id": 0,
+  "incomePayType": "PAYABLE",
+  "incomePayTypeValue": "string",
+  "orgAllId": "string",
+  "senderCompanyName": "string",
+  "senderName": "string",
+  "shipFromCityCode": "string",
+  "shipFromOrgid": 0,
+  "shipLoadId": 0,
+  "shipLoadIdType": 0,
+  "shipSn": "string",
+  "shipToCityCode": "string",
+  "startTime": "2018-07-15T12:38:18.233Z",
+  "status": "string",
+  "totalFee": 0,
+  "totalStatus": "NOSETTLEMENT",
+  "totalStatusValue": "string"
+}
+ */
+export function putChangeFeeReceivable(id, data) {
+  return fetch.put('/api-finance/finance/feeReceivable/v1/' + id, data)
+}
+/**
+ * 根据ID删除tms应收应付表信息
+ * @param {*} id 应付id
+ */
+export function deleteFeeReceivable(id) {
+  return fetch.delete('/api-finance/finance/feeReceivable/v1/' + id)
+}
+/**
+ * 应收结算
+ * @param {*} orgid 组织id
+ * @param {*} data 创建数据
+{
+  "capitalFlow": {
+    "amount": 0,
+    "createBy": 0,
+    "detailDtoList": [
+      {
+        "amount": 0,
+        "arrCardPay": "string",
+        "arrSendPay": "string",
+        "arrivalPay": "string",
+        "backCardPa": "string",
+        "backPay": "string",
+        "backSendPa": "string",
+        "endLoadPay": "string",
+        "endOtherPay": "string",
+        "exceptionPay": "string",
+        "flowId": 0,
+        "id": 0,
+        "kickBackPay": "string",
+        "mainBatchNo": "string",
+        "monthPay": "string",
+        "onCardPay": "string",
+        "onPay": "string",
+        "onSendPay": "string",
+        "othePay": "string",
+        "pickPuPay": "string",
+        "remark": "string",
+        "sendBatchNo": "string",
+        "sendPay": "string",
+        "shipOrderId": 0,
+        "shipOrderType": 0,
+        "shipSn": "string",
+        "shortBatchNo": "string",
+        "shortPay": "string",
+        "startLoadPay": "string",
+        "startOtherPay": "string",
+        "transferPay": "string",
+        "unusualPay": "string"
+      }
+    ],
+    "id": 0,
+    "orgId": 0,
+    "paymentsType": 0,
+    "remark": "string",
+    "settlementBy": "string",
+    "settlementId": 0,
+    "settlementSn": "string",
+    "settlementTime": "2018-07-15T12:41:33.091Z",
+    "szDtoList": [
+      {
+        "agent": "string",
+        "alipayAccount": "string",
+        "bankAccount": "string",
+        "bankAccountName": "string",
+        "bankName": "string",
+        "chequeNumber": "string",
+        "financialWay": "string",
+        "flowId": 0,
+        "id": 0,
+        "receivableNumber": "string",
+        "rnum": 0,
+        "wechatAccount": "string"
+      }
+    ]
+  },
+  "financialWayLogs": [
+    {
+      "agent": "string",
+      "alipayAccount": "string",
+      "bankAccount": "string",
+      "bankAccountName": "string",
+      "bankName": "string",
+      "chequeNumber": "string",
+      "financialWay": "string",
+      "flowId": 0,
+      "id": 0,
+      "receivableNumber": "string",
+      "rnum": 0,
+      "wechatAccount": "string"
+    }
+  ],
+  "receivableFees": [
+    {
+      "inputArrivepayFee": 0,
+      "inputChangeFee": 0,
+      "inputMonthpayFee": 0,
+      "inputNowPayFee": 0,
+      "inputReceiptpayFee": 0,
+      "shipId": 0
+    }
+  ]
+}
+ */
+export function postCreateFee(orgid, data) {
+  return fetch.put('/api-finance/finance/feeReceivable/v1/create/' + orgid, data)
+}
+/**
+ * 判断到站装卸费和到站其他费是否有结算(typeId是28为到站装卸费，29是到站其他费，返回为true时才可修改)
+ * @param {*} loadId 配载id
+ * @param {*} typeId 类型id
+ */
+export function getFeeStatus(loadId, typeId = '') {
+  return fetch.get('/api-finance/finance/feeReceivable/v1/getFeeStatus/' + loadId, {
+    params: {
+      typeId
+    }
+  })
+}
+/**
+ * 根据运单id查询应付应付表的结算状态，为true才可修改运单费用
+ * @param {*} shipId 运单id
+ */
+export function getFeeStatusByShipId(shipId) {
+  return fetch.get('/api-finance/finance/feeReceivable/v1/getStatusByShipId/' + shipId)
+}
+/**
+ * 根据费用类型查询应收费用列表,不传费用类型查询全部
+ * @param {*} data 搜索参数
+{
+  "currentPage": 0,
+  "pageSize": 0,
+  "vo": {
+    "endTime": "string",
+    "feeType": 0,
+    "orgAllId": "string",
+    "senderCompanyName": "string",
+    "senderName": "string",
+    "shipFromCityCode": "string",
+    "shipFromOrgid": 0,
+    "shipSn": "string",
+    "shipToCityCode": "string",
+    "startTime": "string",
+    "status": "NOSETTLEMENT"
+  }
+}
+ */
+export function getReceivableList(data) {
+  return fetch.post('/api-finance/finance/feeReceivable/v1/list', data).then(res => {
+    return res.data || { list: [], totalCount: 0 }
+  })
+}
+/**
+ * 到车入库后修改到站装卸费和到站其他费
+ * @param {*} data 费用信息
+{
+  "arriveHandlingFee": 0,
+  "arriveOtherFee": 0,
+  "arrivepayCarriage": 0,
+  "arrivepayOilCard": 0,
+  "backpayCarriage": 0,
+  "backpayOilCard": 0,
+  "carloadInsuranceFee": 0,
+  "deliveryFee": 0,
+  "id": 0,
+  "leaveHandlingFee": 0,
+  "leaveOtherFee": 0,
+  "loadId": 0,
+  "nowpayCarriage": 0,
+  "nowpayOilCard": 0,
+  "shortFee": 0
+}
+ */
+export function postSaveLoadFee(data) {
+  return fetch.post('/api-finance/finance/feeReceivable/v1/saveLoadFee', data)
+}
+
+/**
+ * ================================》应收明细《================
+ */
+
+ /**
+  * 插入tms应收应付明细(收支费用明细)表信息
+{
+  "arrivePayFee": 0,
+  "ascriptionOrgid": 0,
+  "changeFee": 0,
+  "createTime": "2018-07-15T12:19:54.477Z",
+  "fee": 0,
+  "feeTypeId": 0,
+  "financeFeeId": 0,
+  "id": 0,
+  "incomePayType": "PAYABLE",
+  "monthPayFee": 0,
+  "nowPayFee": 0,
+  "receiptPayFee": 0,
+  "remark": "string",
+  "shipLoadId": 0,
+  "signOrgid": 0,
+  "status": "NOSETTLEMENT",
+  "statusValue": "string",
+  "unpaidFee": 0
+}
+  */
+export function postNewFeeDetail(data) {
+  return fetch.post('/api-finance/finance/tmsfinancefeedetailreceivable/v1/', data)
+}
+/**
+ * 根据ID查询tms应收应付明细(收支费用明细)表信息
+ * @param {*} id 明细信息id
+ */
+export function getFeeDetail(id) {
+  return fetch.get('/api-finance/finance/tmsfinancefeedetailreceivable/v1/').then(res => {
+    return res.data || { list: [], totalCount: 0 }
+  })
+}
+
+  /**
+   * 根据ID修改tms应收应付明细(收支费用明细)表信息
+   * id 明细信息id
+   * @param {*} data 同新建
+   */
+export function putChangeFeeDetail(id, data) {
+  return fetch.put('/api-finance/finance/tmsfinancefeedetailreceivable/v1/' +
+id, data)
+}
+
+/**
+ * 根据ID删除tms应收应付明细(收支费用明细)表信息
+ * @param {*} id 明细信息id
+ */
+export function deleteFeeDetail(id) {
+  return fetch.delete('/api-finance/finance/tmsfinancefeedetailreceivable/v1/' +
+  id)
+}
+
+/**
+ * 根据shipID和网点id,签收修改到付费用结算网点
+ * @param {*} data 修改信息
+{
+  "arrivePayFee": 0,
+  "ascriptionOrgid": 0,
+  "changeFee": 0,
+  "createTime": "2018-07-15T12:27:18.589Z",
+  "fee": 0,
+  "feeTypeId": 0,
+  "financeFeeId": 0,
+  "id": 0,
+  "incomePayType": "PAYABLE",
+  "monthPayFee": 0,
+  "nowPayFee": 0,
+  "receiptPayFee": 0,
+  "remark": "string",
+  "shipLoadId": 0,
+  "signOrgid": 0,
+  "status": "NOSETTLEMENT",
+  "statusValue": "string",
+  "unpaidFee": 0
+}
+ */
+export function putChangeFromSign(data) {
+  return fetch.put('/api-finance/finance/tmsfinancefeedetailreceivable/v1/updateFromSign', data)
+}
--- src/views/finance/accountsReceivable/all.vue
+++ src/views/finance/accountsReceivable/all.vue
@@ -1,5 +1,254 @@
 <template>
-  <div>
-all
+  <div class="tab-content accountsReceivable-summary" v-loading="loading">
+    <SearchForm :orgid="otherinfo.orgid" @change="getSearchParam" :btnsize="btnsize" />  
+    <div class="tab_info">
+      <div class="btns_box">
+          <el-button type="primary" :size="btnsize" icon="el-icon-info" plain @click="doAction('detail')">结算</el-button>
+          
+          <el-button type="primary" :size="btnsize" icon="el-icon-edit-outline" class="table_setup" @click="doAction('export')" plain>导出</el-button>
+          <el-button type="primary" :size="btnsize" icon="el-icon-edit-outline" class="table_setup" @click="doAction('print')" plain>打印</el-button>
+          <el-button type="primary" :size="btnsize" icon="el-icon-setting" plain @click="setTable" class="table_setup">表格设置</el-button>
+      </div>
+      <div class="info_tab">
+        <el-table
+          ref="multipleTable"
+          :data="usersArr"
+          stripe
+          border
+          @row-click="clickDetails"
+          @row-dblclick="showDetail"
+          @selection-change="getSelection"
+          height="100%"
+          show-summary
+          :summary-method="getSummaries"
+          tooltip-effect="dark"
+          :default-sort = "{prop: 'id', order: 'ascending'}"
+          style="width: 100%">
+          
+          <el-table-column
+            fixed
+            sortable
+            type="selection"
+            width="80">
+          </el-table-column>
+          <template v-for="column in tableColumn">
+            <el-table-column
+              :key="column.id"
+              :fixed="column.fixed"
+              sortable
+              :label="column.label"
+              :prop="column.prop"
+              v-if="!column.slot"
+              :width="column.width">
+            </el-table-column>
+            <el-table-column
+              :key="column.id"
+              :fixed="column.fixed"
+              sortable
+              :label="column.label"
+              v-else
+              :width="column.width">
+              <template slot-scope="scope" v-html="true">
+                  {{ column.slot(scope) }}
+              </template>
+            </el-table-column>
+          </template>
+        </el-table>
+      </div>   
+    </div>
+    <TableSetup :popVisible="setupTableVisible" @close="closeSetupTable" @success="fetchData"  />
   </div>
 </template>
+<script>
+import * as accountApi from '@/api/finance/accountsReceivable'
+import SearchForm from './components/search'
+import TableSetup from './components/tableSetup'
+import Pager from '@/components/Pagination/index'
+
+export default {
+  components: {
+    SearchForm,
+    Pager,
+    TableSetup
+  },
+  mounted() {
+    // this.loading = false
+  },
+  data() {
+    return {
+      btnsize: 'mini',
+      usersArr: [],
+      total: 0,
+      // 加载状态
+      loading: true,
+      setupTableVisible: false,
+      isModify: false,
+      selectInfo: {},
+      // 选中的行
+      selected: [],
+      searchQuery: {
+        'currentPage': 1,
+        'pageSize': 100,
+        'vo': {
+          shipFromOrgid: '',
+          endTime: '',
+          startTime: '',
+          senderName: '',
+          senderCompanyName: '',
+          shipFromCityCode: '',
+          shipToCityCode: '',
+          shipSn: '',
+          'status': 'NOSETTLEMENT',
+          'feeType': 0
+        }
+      },
+      // 默认sort值为true
+      tableColumn: [{
+        'label': '序号',
+        'prop': '',
+        'fixed': true,
+        'slot': function(scope) {
+          return scope.$index + 1
+        }
+      }, {
+        'label': '网点',
+        'prop': 'orgName',
+        'fixed': true
+      }, {
+        'label': '员工姓名',
+        'prop': 'name'
+      }, {
+        'label': '交账合计',
+        'prop': 'shipNowpayFee'
+      }, {
+        'label': '交账状态',
+        'prop': 'feeStatus',
+        'width': '150'
+      }, {
+        'label': '已交账合计',
+        'prop': 'finishAccount'
+      }, {
+        'label': '未交账合记',
+        'prop': 'noSettlementFee'
+      }]
+    }
+  },
+  methods: {
+    viewDetails(row) {
+      this.$router.push({
+        path: '/finance/handAccount/detail',
+        query: {
+          id: row.userId,
+          tab: '查看' + row.name
+        }
+      })
+    },
+    showDetail(order) {
+      this.viewDetails(order)
+    },
+    fetchAllOrder() {
+      this.loading = true
+      return accountApi.getReceivableList(this.searchQuery).then(data => {
+        this.usersArr = data.list
+        this.total = data.total
+        this.loading = false
+      })
+    },
+    fetchData() {
+      this.fetchAllOrder()
+    },
+    handlePageChange(obj) {
+      this.searchQuery.currentPage = obj.pageNum
+      this.searchQuery.pageSize = obj.pageSize
+      this.fetchData()
+    },
+    getSearchParam(obj) {
+      this.searchQuery.vo = Object.assign(this.searchQuery.vo, obj)
+      this.loading = false
+      this.fetchData()
+    },
+    doAction(type) {
+      // 判断是否有选中项
+      if (!this.selected.length && type !== 'add') {
+        this.$message({
+          message: '请选择要操作的项~',
+          type: 'warning'
+        })
+        return false
+      }
+
+      console.log('this.selected:', this.selected)
+
+      switch (type) {
+          // 添加运单
+        case 'detail':
+          this.isModify = false
+          this.selectInfo = {}
+          this.showDetail(this.selected[0])
+          break
+          // 导出数据
+        case 'export':
+          var ids2 = this.selected.map(el => {
+            return el.customerId
+          })
+          accountApi.getExportExcel(ids2.join(',')).then(res => {
+            this.$message({
+              type: 'success',
+              message: '即将自动下载!'
+            })
+          })
+          break
+      }
+      // 清除选中状态，避免影响下个操作
+      this.$refs.multipleTable.clearSelection()
+    },
+    setTable() {
+      this.setupTableVisible = true
+    },
+    closeSetupTable() {
+      this.setupTableVisible = false
+    },
+    clickDetails(row, event, column) {
+      this.$refs.multipleTable.toggleRowSelection(row)
+    },
+    getSelection(selection) {
+      this.selected = selection
+    },
+    // 计算总数
+    getSummaries(param) {
+      const { columns, data } = param
+      const sums = []
+      // console.log(columns, data)
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = '总计'
+          return
+        }
+        const values = data.map(item => Number(item[column.property]))
+        if (!values.every(value => isNaN(value))) {
+          sums[index] = values.reduce((prev, curr) => {
+            const value = Number(curr)
+            if (!isNaN(value)) {
+              return prev + curr
+            } else {
+              return prev
+            }
+          }, 0)
+          sums[index] += ' 元'
+        } else {
+          sums[index] = 'N/A'
+        }
+      })
+
+      return sums
+    }
+  }
+}
+</script>
+<style lang="scss">
+.accountsReceivable-manager .tab-content{
+  .tab_info{
+    padding-bottom: 10px;
+  }
+}
+</style>
