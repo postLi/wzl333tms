aa56741c41d0db34e17ea2d81f28ac35c5c045c8
lyy <lyy6217@163.com>
2018-07-21 16:08:16 +0800

    硕正插件学习版

--- src/views/report/settleRecordTotal/index.vue
+++ src/views/report/settleRecordTotal/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>应收应付汇总表</div>
+  <!-- 应收应付汇总表 -->
+  <div class="tab-wrapper tab-wrapper-100">
+    <keep-alive v-bind:is="component"></keep-alive>
+  </div>
 </template>
+<script>
+import sender from './sender'
+export default {
+  components: {
+    sender
+  },
+  data() {
+    return {
+      component: 'sender'
+    }
+  }
+}
+
+</script>
---
+++ src/views/report/settleRecordTotal/sender.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="chartSender">
+    <iframe :src="gameIframe" ref="showIframe" frameborder='0' scrolling=auto name="showHere" class="chartIframe"></iframe>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      gameIframe: ''
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      let path = window.location.protocol + '//' + window.location.host + '/static/supcan/demo.html'
+      this.gameIframe = path
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+.chartSender{
+  width:100%;
+  height: calc(100% - 10px);
+.chartIframe{
+  width:100%;
+  height:100%;
+}
+}
+
+</style>
