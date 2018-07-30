78980cba397599f000da0c26e45cf6531e67acd2
何永青 <864887652@qq.com>
2018-07-30 10:31:22 +0800

    添加分析命令

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,7 +23,8 @@ var webpackConfig = merge(baseWebpackConfig, {
       extract: true
     })
   },
-  devtool: config.build.productionSourceMap ? '#source-map' : false,
+ // devtool: config.build.productionSourceMap ? '#source-map' : false,
+  devtool: false,
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
