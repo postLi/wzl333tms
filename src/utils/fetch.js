61754572a301302f5a247cf855d8826f1eb9444d
何永青 <864887652@qq.com>
2018-08-16 10:08:44 +0800

    修改

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -32,16 +32,24 @@ service.interceptors.request.use(config => {
        * 测试环境修改这里，不要修改上面那句代码
        */
       // 统一加上/api 前缀，方便后台转发接口
-      config.url = '/api' + config.url
-      // config.url = '/localapi' + config.url
-      // config.url = '/wukunzhi' + config.url
-      // config.url = '/huangyuwen' + config.url
-      // config.url = '/dingfei' + config.url
-      // config.url = '/ceshi' + config.url
-      // config.url = '/chenrongtao' + config.url
-      // config.url = '/home' + config.url
-      // config.url = '/fangjian' + config.url
-      config.url = '/aliyun' + config.url
+      
+      window.tms_testapiurl = localStorage.tms_testapiurl || 'api'
+      
+      if(window.tms_testapiurl){
+        config.url = '/' + window.tms_testapiurl + config.url
+      } else {
+        
+        // config.url = '/api' + config.url
+        // config.url = '/localapi' + config.url
+        config.url = '/wukunzhi' + config.url
+        // config.url = '/huangyuwen' + config.url
+        // config.url = '/dingfei' + config.url
+        // config.url = '/ceshi' + config.url
+        // config.url = '/chenrongtao' + config.url
+        // config.url = '/home' + config.url
+        // config.url = '/fangjian' + config.url
+        // config.url = '/aliyun' + config.url
+      }
     }
   }
 
