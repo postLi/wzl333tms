8640324f22d51b9d13097982268cdc7df1ca0c04
lyy <lyy6217@163.com>
2018-08-23 10:53:47 +0800

    修改登录界面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,62 +1,54 @@
 <template>
+
   <div class="login-container">
-    <div class="login-wrapper clearfix">
-    <div class="container-left">
-      <img src="../../assets/login_images/left.png" alt="">
+    <div class="container-top">
+      <div class="login-wrapper clearfix">
+        <img  class="logo-img" src="../../assets/login_images/login_03.png" alt="">
+      </div>
     </div>
-    <div class="container-right">
-        <div class="logo">
+    <div class="login-wrapper clearfix">
+
+      <!-- <div class="container-left">
+      <img src="../../assets/login_images/left.png" alt="">
+      </div> -->
+      <div class="container-right">
+        <!--  <div class="logo">
           <img  class="logo-img" src="../../assets/login_images/logo.png" alt="">
-        </div>
-
-      <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
-               class="card-box login-form">
-        <h3 class="title">欢迎使用安发物流</h3>
-
-        <!--<div v-if="errInfo">-->
+        </div> -->
+        <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px" class="card-box login-form">
+          <h3 class="title">会员登录</h3>
+          <!--<div v-if="errInfo">-->
           <!--<span>{{errInfo}}</span>-->
-        <!--</div>-->
-
-
-        <!-- <el-form-item prop="accNum"> -->
-
+          <!--</div>-->
+          <!-- <el-form-item prop="accNum"> -->
           <!--<span class="svg-container svg-container_login">-->
           <!--<icon-svg icon-class="yonghuming" />-->
           <!--</span>-->
-
-         <!-- <el-input name="accNum" type="text" v-model="loginForm.accNum" autoComplete="off" :placeholder="holder.accNum" @focus='accNum()'   clearable >
+          <!-- <el-input name="accNum" type="text" v-model="loginForm.accNum" autoComplete="off" :placeholder="holder.accNum" @focus='accNum()'   clearable >
           <template slot="prepend">公司ID</template>
          </el-input>
         </el-form-item> -->
-
-        <el-form-item prop="username">
-          <el-input name="username" type="text" @keyup.enter.native="handleLogin" v-model="loginForm.username" autoComplete="off"
-                    :placeholder="holder.username" @focus='username()' clearable>
-                    <template slot="prepend">用户名</template>
-         </el-input>
-
-
-        </el-form-item>
-
-        <el-form-item prop="password">
-          <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="off"
-                    :placeholder="holder.password" @focus='password()' clearable>
-            <template slot="prepend">密　码</template>
-          </el-input>
-        </el-form-item>
-
-        <el-form-item class="login">
-          <el-button class="el-but" type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
-            登录
-          </el-button>
-        </el-form-item>
-
-        <div class="rember">
-          <el-checkbox v-model="checked">记住密码</el-checkbox>
-          <p class="rember-tit" @click="forgetPsw">忘记密码</p>
-        </div>
-      </el-form>
-    </div>
+          <el-form-item prop="username">
+            <el-input name="username" type="text" @keyup.enter.native="handleLogin" v-model="loginForm.username" autoComplete="off" :placeholder="holder.username" @focus='username()' clearable>
+              <template slot="prepend"><i class="icon_login icon_login_user"></i></template>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="off" :placeholder="holder.password" @focus='password()' clearable>
+              <template slot="prepend"><i class="icon_login icon_login_password"></i></template>
+            </el-input>
+          </el-form-item>
+          <el-form-item class="login">
+            <el-button class="el-but" type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+              登录
+            </el-button>
+          </el-form-item>
+          <div class="rember">
+            <el-checkbox v-model="checked">记住密码</el-checkbox>
+            <p class="rember-tit" @click="forgetPsw">忘记密码</p>
+          </div>
+        </el-form>
+      </div>
     </div>
     <div class="button-cont">
       <ul>
@@ -73,7 +65,6 @@
     <setApiUrl />
   </div>
 </template>
-
 <script>
 import { isvalidUsername } from '@/utils/validate'
 import { requestFullScreen } from '@/utils/fullScreen'
@@ -81,7 +72,7 @@ import setApiUrl from '@/components/changeApiUrl/index'
 
 export default {
   name: 'login',
-  components:{
+  components: {
     setApiUrl
   },
   data() {
@@ -128,6 +119,11 @@ export default {
         // username: [{ required: true, trigger: 'blur', validator: validateUsername }],
         password: [{ required: true, trigger: 'blur', validator: validatePass }]
       }
+
+    }
+  },
+  computed: {
+    iconLoginUser: {
 
     }
   },
@@ -183,11 +179,10 @@ export default {
     }
   }
 }
+
 </script>
-
 <style rel="stylesheet/scss" lang="scss">
-  @import "../../styles/mixin.scss";
-  @import "../../styles/login-index.css";
-
+@import "../../styles/mixin.scss";
+@import "../../styles/login-index.css";
 
 </style>
