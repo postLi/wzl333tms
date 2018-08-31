52057a89f333fc08bb38783f7fdf0c634f3052b4
postLi <1285161341@qq.com>
2018-08-31 09:39:37 +0800

    拉取代码

--- src/views/operation/order/sign/pick.vue
+++ src/views/operation/order/sign/pick.vue
@@ -485,6 +485,7 @@
               </template>
             </el-table-column>
           </template>
+          
         </el-table>
       </div>
       <div class="info_tab_footer">共计:{{ total}} <div class="show_pager"> <Pager :total="total" @change="handlePageChange" /></div> </div>
