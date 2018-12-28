094e6893328c59fa33cc249c77e5da80996e6c2f
何永青 <864887652@qq.com>
2018-12-28 16:31:20 +0800

    v1.4.5

--- dist/static/js/387.596768bdc04ebe51363c.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([387],{JpB7:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("KnWN"),l=t("VU/8")(null,r.a,null,null,null);n.default=l.exports},KnWN:function(e,n,t){"use strict";var r={render:function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  客服页面\n")])},staticRenderFns:[]};n.a=r}});
---
+++ src/views/layout/AppMain2.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="app-main">
+      <keep-alive :include="cachedViews">
+        <router-view></router-view>
+      </keep-alive>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  computed: {
+    cachedViews() {
+      return this.$store.state.tagsView.cachedViews
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
