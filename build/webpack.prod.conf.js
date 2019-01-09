a7233b39cc534886fb691f7039881b018ca50710
何永青 <864887652@qq.com>
2019-01-09 11:49:59 +0800

    调整打包策略

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,8 +106,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       chunks:['app'],
-      minChunks: 3,
-      /* minChunks: function (module, count) {
+      // minChunks: Infinity,
+      minChunks: function (module, count) {
 
         // any required modules inside node_modules are extracted to vendor
         return (
@@ -117,13 +117,19 @@ var webpackConfig = merge(baseWebpackConfig, {
             path.join(__dirname, '../node_modules')
           ) === 0
         )
-      } */
+      }
+    }),
+    // 提取公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      children: true,
+      async: 'children-async'
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'polyfills',
       filename: 'polyfills.js',
       chunks:['polyfills']
     }),
+    
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
