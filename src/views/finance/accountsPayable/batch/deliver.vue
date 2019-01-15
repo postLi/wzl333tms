5e8aa258da067c8ccf87426e22c15051bf74cd66
lyy <lyy6217@163.com>
2019-01-15 18:55:45 +0800

    中转备注长度修改

--- src/views/finance/accountsPayable/batch/deliver.vue
+++ src/views/finance/accountsPayable/batch/deliver.vue
@@ -13,7 +13,8 @@
       </div>
       <!-- 数据表格 -->
       <div class="info_tab">
-        <el-table ref="multipleTable" :key="tablekey" :data="dataList" stripe border @row-click="clickDetails" @selection-change="getSelection" height="100%" tooltip-effect="dark" style="width:100%;" @cell-dblclick="showDetail" :show-summary="true" :summary-method="getSummaries">
+        <el-table ref="multipleTable" :key="tablekey" :data="dataList" stripe border @row-click="clickDetails" @selection-change="getSelection" height="100%" tooltip-effect="dark" style="width:100%;" @cell-dblclick="showDetail"
+         :show-summary="true" :summary-method="getSummaries">
           <el-table-column fixed sortable type="selection" width="35">
           </el-table-column>
           <template v-for="column in tableColumn">
